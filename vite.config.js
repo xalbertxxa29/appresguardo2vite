a087cbf64bbff13c20fac3c05513742c4b671e05
Juan Alberto Solis <jsolis@liderman.com>
2026-03-04 18:57:16 -0500

    Ajustes para WebViewer y corrección de color de sirena

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/appresguardo2vite/',
+  base: './',
   build: {
     rollupOptions: {
       input: {
